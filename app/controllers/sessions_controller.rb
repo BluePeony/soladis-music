3ea78c0d0767c073a72975c6778b91bba72e10bf
Anna <anna.foerster1@gmx.net>
2021-10-27 14:23:20 +0200

    working log_in and current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		reset_session
+  		log_in user
+  		redirect_to user
+		else
+			if !user
+				flash.now[:danger] = "E-Mail Adresse ist unbekannt."
+			elsif !user.authenticate(params[:session][:password])
+				flash.now[:danger] = "Password ist ungültig."
+			end
+			render 'new'
+		end
+  end
+
+  def destroy
+  end
 end
