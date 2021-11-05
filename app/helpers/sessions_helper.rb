152cf09ef84a246081e1a28768241a5c0c53fac3
Anna <anna.foerster1@gmx.net>
2021-11-05 13:13:12 +0100

    incorporated remember cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,38 @@ module SessionsHelper
 	# Logs in the given user
 	def log_in(user)
 		session[:user_id] = user.id
+		# Guard against session replay attacks.
+		session[:session_token] = user.session_token
+	end
+
+	# Remembers a user in a persistent session
+	def remember(user)
+		user.remember
+		cookies.permanent.encrypted[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	# Forgets a user in a pesistent session
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
 	end
 
 	# Returns the current logged-in user (if any)
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
+		if (user_id = session[:user_id])
+			user = User.find_by(id: user_id)
+			if user && session[:session_token] = user.remember_digest
+				@current_user user
+			end
+		elsif (user_id = cookies.encrypted[:user_id])
+			user = User.find_by(id: user_id)
+			if user&.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
 	end
 
 	# Returns true if the user is logged in, false otherwise
@@ -17,6 +44,7 @@ module SessionsHelper
 
 	# Logs out the current user
 	def log_out
+		forget(current_user)
 		reset_session
 		@current_user = nil
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -7,9 +9,41 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 8 }
 
+	# Remembers a user in the database for use in persistent sessions.
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(self.remember_token))
+		remember_digest
+	end
+
+	# Returns a session token to prevent session hijacking.
+	# We reuse the remember digest for convenience.
+	def session_token
+		remember_digest || remember
+	end
+
 	# Returns the hash digest of the given string
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+	end
+
+	# Returns a random token
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	# Returns true if the user is authenticated - if the remember token corresponds to the remember digest
+	def authenticated?(remember_token)
+		if remember_digest
+			BCrypt::Password.new(remember_digest).is_password?(remember_token)
+		else
+			return false
+		end
+	end
+
+	# Forgets a user
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 end
