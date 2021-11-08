0262a64d1156a95e3642c959a57887ce4e0ac58e
Anna <anna.foerster1@gmx.net>
2021-11-08 13:25:55 +0100

    fixed an error

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,7 +26,7 @@ module SessionsHelper
 		if (user_id = session[:user_id])
 			user = User.find_by(id: user_id)
 			if user && session[:session_token] = user.remember_digest
-				@current_user user
+				@current_user = user
 			end
 		elsif (user_id = cookies.encrypted[:user_id])
 			user = User.find_by(id: user_id)
