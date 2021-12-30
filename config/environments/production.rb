a69b2c82e8493f999ff6927449ee144477914984
Anna <anna.foerster1@gmx.net>
2021-12-30 14:15:41 +0100

    changed heroku url in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method= :smtp
-  host = 'https://glacial-spire-85470.herokuapp.com/'
+  host = 'https://pure-woodland-91572.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',
