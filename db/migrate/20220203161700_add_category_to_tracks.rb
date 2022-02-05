03f40bcb278e95d9b91fa8130f2b88514e7a6baa
Anna <anna.foerster1@gmx.net>
2022-02-05 13:38:02 +0100

    corrected migration file

--- db/migrate/20220203161700_add_category_to_tracks.rb
+++ db/migrate/20220203161700_add_category_to_tracks.rb
@@ -1,5 +1,5 @@
 class AddCategoryToTracks < ActiveRecord::Migration[6.1]
   def change
-    add_reference :tracks, :category foreign_key: true
+    add_reference :tracks, :category, foreign_key: true
   end
 end
